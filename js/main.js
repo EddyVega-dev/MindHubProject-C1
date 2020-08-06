158a651d89d29a3ed9ab6d99d6378cadfc6e2de1
Eddy Vega <evegag@fi.uba.ar>
2020-08-06 04:56:14 -0300

    Se arregló detalles en contact para una mejor presentación del formulario.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    M.Parallax.init(elems);
-    var elems2 = document.querySelectorAll('.datapicker');
-    M.Datepicker.init(elems2);
+var app = new Vue({
+  el: '#app',
+  data: {
+    elemento1: 0,
+  }
+})
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $('.datapicker').datepicker();
+  $('.dropdown-trigger').dropdown({
+    hover: true,
+    alignment: 'left',
+    constrainWidth: false,
+    coverTrigger: false,
   });
+  $('.submit').click(function(){
+    Swal({
+      title: 'holo',
+    })
+  });
+  $.ajax({
+    url: `https://apipetshop.herokuapp.com/api/articulos`,
+    success: function(info){
+      
+    }
+  })
+})
