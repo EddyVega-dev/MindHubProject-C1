b14ca465a8399f118f6d0f89ec4fbdc6b8013499
Eddy Vega <evegag@fi.uba.ar>
2020-08-06 21:22:18 -0300

    Gran avance con las alertas en el formulario.
    Sale un sweetAlert si falta algún input relevante y sale una alerta conforme si están completas junto a un refresco de la página si apreta la confirmación.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,81 @@
 var app = new Vue({
   el: '#app',
   data: {
-    elemento1: 0,
+    arrayProducts: [],    
   }
 })
 
-$(document).ready(function(){
-  $('.parallax').parallax();
-  $('.datapicker').datepicker();
-  $('.dropdown-trigger').dropdown({
-    hover: true,
-    alignment: 'left',
-    constrainWidth: false,
-    coverTrigger: false,
-  });
-  $('.submit').click(function(){
-    Swal({
-      title: 'holo',
-    })
-  });
-  $.ajax({
-    url: `https://apipetshop.herokuapp.com/api/articulos`,
-    success: function(info){
-      
-    }
+function getActiveLetter(){
+  let letterActive = '';
+  if($(".active").text() == 'local_pharmacyPharmacy') letterActive = 'Medicamento';
+  if($(".active").text() == 'petsToys') letterActive = 'Juguete';
+  return letterActive;
+}  
+
+function thenDataStatics(anProducts){
+  anProducts.map(product => {
+    if(product.tipo == getActiveLetter()) app.arrayProducts.push(product);
   })
-})
+}
+
+function main(){
+  $(document).ready(function(){
+    $('.parallax').parallax();
+    $('.datapicker').datepicker();
+    $('.dropdown-trigger').dropdown({
+      hover: true,
+      alignment: 'left',
+      constrainWidth: false,
+      coverTrigger: false,
+    });
+    $('#submit').click(function(){
+      var nameUser = $('#first_name').val();
+      var lastNameUser = $('#last_name').val();
+      var emailUser = $('#email').val();
+      var phone = $('#phone').val();
+      var namePet = $('#name_pet').val();
+      var agePet = $('#age_pet').val();
+
+      console.log(typeof(agePet));
+      if(nameUser == '' || lastNameUser == '' || emailUser == '' || phone == '' || namePet == '' || agePet == ''){
+        Swal.fire({
+          title: 'WARNING!!!',
+          confirmButtonText: 'Confirm',
+          timer:10000,
+          timerProgressBar: true,
+          footer: 'Missing relevant information.',
+          customClass: {
+            title: 'title-class',
+          },
+          imageUrl: '../assets/alertWarning.png',
+          imageWidth: '20vw',
+          imageAlt: 'alertWarnning',
+        })
+      }
+      else{
+        Swal.fire({
+          title: 'READY!!!',
+          confirmButtonText: 'Confirm',
+          timer:10000,
+          timerProgressBar: true,
+          footer: 'Complete, congratulation!!',
+          imageUrl: '../assets/alertConfirm.png',
+          imageWidth: '20vw',
+          imageAlt: 'alertConfirm',
+        }).then((result) => {
+          if(result.value) {
+            location.reload();
+          }
+        })
+      }
+    });
+    $.ajax({
+      url: `https://apipetshop.herokuapp.com/api/articulos`,
+      success: function(info){
+        thenDataStatics(info.response);
+      }
+    })
+  })
+};
+
+main();
